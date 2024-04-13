9e7ca46ecfadafbd19ddb88907750d353b908748
Mohit Bhatt <bhattmohit869@gmail.com>
2024-04-13 09:56:58 +0530

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center mt-5 md:mt-20 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-8 text-blue-600 [text-shadow:_4px_4px_10px_rgb(100_0_0_/_40%)]">Stock Data</h1>
-      <div className="w-full max-w-md bg-violet-400 opacity-85 shadow-lg rounded-lg p-6">
+    <div className="flex flex-col items-center justify-center mt-5 md:mt-20 bg-inherit">
+      <h1 className="text-3xl font-bold mb-8 text-orange-400 [text-shadow:_4px_4px_10px_rgb(255_255_255_/_40%)]">Stock Data</h1>
+      <div className="w-full max-w-md bg-violet-600 opacity-85 shadow-lg rounded-lg p-6">
         <StockSelect
           stocks={stockData}
           selectedStock={selectedStock}
